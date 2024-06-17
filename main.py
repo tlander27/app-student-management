4c4298509f27cd5f204a70b2937fc5eb6d3e984c
joshua <jrt@tellander.net>
2024-06-16 21:19:07 -0500

    Add insert dialog window, sqlite3 connection

--- main.py
+++ main.py
@@ -1,32 +1,98 @@
 from PyQt6.QtWidgets import QApplication, QLabel, \
     QWidget, QGridLayout, QLineEdit, QPushButton, \
-    QMainWindow, QTableWidget
+    QMainWindow, QTableWidget, QTableWidgetItem, \
+    QDialog, QVBoxLayout, QComboBox
 from PyQt6.QtGui import QAction
 import sys
-from datetime import datetime
+import sqlite3 as sql
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Student Management System")
-
+        self.setFixedSize(480, 380)
+        # Create root menu options
         file_menu_item = self.menuBar().addMenu("&File")
         help_menu_item = self.menuBar().addMenu("&Help")
 
+        # Add submenu items and actions
         add_student_action = QAction("Add Student", self)
+        add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
 
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
 
+        # Create table layout for data
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
 
     def load_data(self):
-        self.table
+        connection = sql.connect("database.db")
+        results = connection.execute("select * from students")
+        # Avoid overwriting entries
+        self.table.setRowCount(0)
+        # Loop results to add to table widget
+        for row_num, row_data in enumerate(results):
+            self.table.insertRow(row_num)
+            for col_num, data in enumerate(row_data):
+                self.table.setItem(row_num, col_num, QTableWidgetItem(str(data)))
+        connection.close()
+
+    def insert(self):
+        dialog = InsertDialog()
+        dialog.exec()
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Insert Student Data")
+        self.setFixedSize(300, 300)
+
+        layout = QVBoxLayout()
+
+        # Add student name widget
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
+
+        # Add courses combobox list
+        self.course_name = QComboBox()
+        courses = ["Biology", "Software Engineering",
+                   "Math", "Astronomy", "Physics"]
+        self.course_name.addItems(courses)
+        layout.addWidget(self.course_name)
+
+        # Add mobile widget
+        self.mobile = QLineEdit()
+        self.mobile.setPlaceholderText("Mobile")
+        layout.addWidget(self.mobile)
+
+        # Add submit button
+        button = QPushButton("Submit")
+        button.clicked.connect(self.add_student)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+    def add_student(self):
+        name = self.student_name.text()
+        course = self.course_name.itemText(self.course_name.currentIndex())
+        mobile = self.mobile.text()
+
+        connection = sql.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute("insert into students (name, course, mobile) "
+                       "values (?,?,?)", (name, course, mobile))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        window.load_data()
+        # self.close()
 
 
 if __name__ == "__main__":
@@ -35,4 +101,5 @@ if __name__ == "__main__":
     window = MainWindow()
     # Initialize application loop
     window.show()
+    window.load_data()
     sys.exit(app.exec())
